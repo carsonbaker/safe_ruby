7ad389d66ec55be91d90791a7981c278114d48d9
Patrick Auernig <patrick.auernig@mykolab.com>
2016-12-20 17:59:14 +0100

    specify gem version in gemspec

--- lib/safe_ruby.rb
+++ lib/safe_ruby.rb
@@ -3,7 +3,3 @@ require_relative 'method_whitelist'
 require_relative 'constant_whitelist'
 require_relative 'make_safe_code'
 require_relative 'safe_ruby_runner'
-
-class SafeRuby
-	VERSION = "1.0.1"
-end
