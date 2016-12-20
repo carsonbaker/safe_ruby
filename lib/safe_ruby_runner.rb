6cfef99407ded02df92c6f6d3464815b7eb85c2d
Patrick Auernig <patrick.auernig@mykolab.com>
2016-12-20 20:25:47 +0100

    raise TimeoutError instead of printing the message

--- lib/safe_ruby_runner.rb
+++ lib/safe_ruby_runner.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
-class EvalError < StandardError
-  def initialize(msg)
-    super
-  end
-end
+require 'tempfile'
 
 class SafeRuby
-  DEFAULTS = { timeout: 5,
-               raise_errors: true }.freeze
+  EvalError = Class.new(StandardError)
+
+  DEFAULTS = {
+    timeout: 5,
+    raise_errors: true
+  }.freeze
+
+  def self.eval(code, options = {})
+    new(code, options).eval
+  end
+
+  def self.check(code, expected)
+    eval(code) == eval(expected)
+  end
 
   def initialize(code, options = {})
+    raise ArgumentError, 'No code to evaluate.' unless code
+
     options = DEFAULTS.merge(options)
 
     @code         = code
@@ -18,13 +28,10 @@ class SafeRuby
     @timeout      = options[:timeout]
   end
 
-  def self.eval(code, options = {})
-    new(code, options).eval
-  end
-
   def eval
     temp = build_tempfile
     read, write = IO.pipe
+
     ChildProcess.build('ruby', temp.path).tap do |process|
       process.io.stdout = write
       process.io.stderr = write
@@ -33,36 +40,42 @@ class SafeRuby
         process.poll_for_exit(@timeout)
       rescue ChildProcess::TimeoutError => e
         process.stop # tries increasingly harsher methods to kill the process.
-        return e.message
+        raise e
+      ensure
+        write.close
+        temp.unlink
       end
-      write.close
-      temp.unlink
     end
 
     data = read.read
+
     begin
       Marshal.load(data)
     rescue
-      raise data if @raise_errors
-      return data
+      raise data if raise_errors?
+    ensure
+      read.close
     end
-  end
 
-  def self.check(code, expected)
-    eval(code) == eval(expected)
+    data
   end
 
   private
 
   def build_tempfile
-    require 'tempfile'
     file = Tempfile.new('saferuby')
     file.write(MAKE_SAFE_CODE)
+
     file.write <<-STRING
       result = eval(%q(#{@code}))
       print Marshal.dump(result)
     STRING
+
     file.rewind
     file
+  end
+
+  def raise_errors?
+    @raise_errors
   end
 end
